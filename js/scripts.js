d0d5380d380c4fdce16c824ccb12c0980a3ee8b4
Cliff Canan <cliff@nooch.com>
2017-01-03 19:16:58 -0500

    • Added 'Companies' section with info on Nooch and Rent Scene (including Schema.org tags)

--- js/scripts.js
+++ js/scripts.js
@@ -121,10 +121,10 @@ $(document).ready(function () {
     );
 
     $("#text-scroll").typed({
-        strings: [" \"sweat-the-details\" product manager", " fintech entrepreneur", " obsessive front-end developer", " perpetual learner", " mobile payments aficionado", " self-taught coder", " hardened Philadelphia sports fan", " front-end developer", " resilient, scrappy startup vet", " web/mobile app developer", "lways asking \"why?\"", " surprisingly good ping-pong player"],
-        typeSpeed: 16,
-        backSpeed: 5, // backspacing speed
-        backDelay: 2300, // time before backspacing
+        strings: [" \"sweat-the-details\" product manager", " fintech entrepreneur", " front-end developer", " perpetual learner", " mobile payments aficionado", " self-taught coder", " hardened Philadelphia sports fan", " front-end developer", " resilient, scrappy startup vet", " web/mobile app developer", "lways asking \"why?\"", " surprisingly good ping-pong player"],
+        typeSpeed: 6,
+        backSpeed: 4, // backspacing speed
+        backDelay: 2400, // time before backspacing
         loop: true,
         loopCount: 3, // false = infinite
     });
